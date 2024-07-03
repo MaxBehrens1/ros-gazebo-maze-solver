7a42826a9a982415d29f51e461827729f66e8ff6
MaxBehrens1 <maxsbehrens@gmail.com>
2024-07-03 16:14:50 +0100

    Solve basic maze given coordinates

--- robot_controller/robot_controller/basic_closed_loop.py
+++ robot_controller/robot_controller/basic_closed_loop.py
@@ -10,6 +10,7 @@ class basic_closed_loop(Node):
 
     open('src/robot_controller/data/position_data.txt', 'w').close() #to clear file contents
     positions = open('src/robot_controller/data/position_data.txt', 'a')
+    coordinates = np.array([[5,0], [5,-3], [-3,-3], [-3,3], [5,3]])
 
     def __init__(self):
         super().__init__('basic_closed_loop')
@@ -18,11 +19,13 @@ class basic_closed_loop(Node):
             Odometry, '/odom', self.pose_callback, 10)
         self.cmd_vel_publisher = self.create_publisher(
             Twist, '/cmd_vel', 10) 
+        self.i = 0
+        self.correct_bearing = False
                
 
     def pose_callback(self, odom: Odometry):
         cmd = Twist()
-        final_pos = np.array([1,-2])
+        final_pos = self.coordinates[self.i]
         rel_vec_to_pos = np.array([final_pos[0] - odom.pose.pose.position.x, final_pos[1] - odom.pose.pose.position.y])
         dist_to_final_pos = np.sqrt(np.dot(rel_vec_to_pos, rel_vec_to_pos))
         current_bearing = np.arctan2(2 * odom.pose.pose.orientation.w * odom.pose.pose.orientation.z,
@@ -35,15 +38,20 @@ class basic_closed_loop(Node):
         if current_bearing < 0:
             current_bearing += 2 * np.pi
 
-        if np.isclose(current_bearing, bearing_to_final_pos, atol=0.01) == False and dist_to_final_pos >= 0.1:
-            cmd.angular.z = 0.05
+        if np.isclose(current_bearing, bearing_to_final_pos, atol=0.001) == False and self.correct_bearing == False:
+            cmd.angular.z = 0.02
             cmd.linear.x = 0.0
-        elif np.isclose(dist_to_final_pos, 0, atol=0.01) == False:
+        elif np.isclose(dist_to_final_pos, 0, atol=0.05) == False:
+            self.correct_bearing = True
             cmd.angular.z = 0.0
             cmd.linear.x = 0.1
         else:
             cmd.linear.x = 0.0
             cmd.angular.z = 0.0
+            self.correct_bearing = False
+            if self.i < len(self.coordinates)-1:
+                self.i += 1
+
         self.positions.write(f'{odom.pose.pose.position.x}, {odom.pose.pose.position.y} \n')
         self.cmd_vel_publisher.publish(cmd)
         
